f61e3381d7af485e0b3959cef11dd7e8434932c6
Ramesh sharma <liwinghumanz@gmail.com>
2021-10-17 13:08:35 +0530

    admin and db

--- monthlych/views.py
+++ monthlych/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request, HttpResponseRedirect
+from monthlych.models import Register
 
 chal={ "jan":"do Django course",
     "feb": "walk 100 m",
@@ -18,9 +19,10 @@ def home(request):
         college= f_data['college']
         contact = f_data['no']
         loc = f_data['loc']
-        hresp = "<p>"+name+ email +college+contact+loc+ "<p>"
-        return HttpResponse(hresp)
-        #return render(request,'monthlych/index.html',{'data':data,'color':'red'})
+        #hresp = "<p>"+name+ email +college+contact+loc+ "<p>"
+        reg = Register.objects.create(name= name,mailid=email,college= college,contact=contact,loc=loc)
+        #return HttpResponse(hresp)
+        return render(request,'monthlych/index.html',{'data':data,'regdata':reg,'color':'red'})
     #return HttpResponse("<h1>iam inside chal</h1>")
     elif request.method =='GET':
         return render(request,'monthlych/index.html',{'data':data,'color':'black'})
--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'myfirstsite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': ' database name',
+        'USER': ' postgres ',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
     }
-}
+    }
 
 
 # Password validation
